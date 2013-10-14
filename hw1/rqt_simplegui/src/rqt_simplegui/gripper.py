f70f4b997a8f857ee7c402d2ee25371c49c7fab6
Vimala <vjampala@cs.washington.edu>
2013-10-14 12:57:53 -0700

    Deleted brokn warning message

--- hw1/rqt_simplegui/src/rqt_simplegui/gripper.py
+++ hw1/rqt_simplegui/src/rqt_simplegui/gripper.py
@@ -4,6 +4,7 @@ roslib.load_manifest('rospy')
 roslib.load_manifest('actionlib')
 roslib.load_manifest('actionlib_msgs')
 roslib.load_manifest('control_msgs')
+roslib.load_manifest('pr2_controllers_msgs')
 import rospy
 from control_msgs.msg import GripperCommandAction
 from control_msgs.msg import GripperCommandGoal
@@ -23,35 +24,33 @@ class Gripper():
         self.direction = direction
         self.state = state
          
+    
     def create_closure(self):
         
         def move_gripper():
-            name_space = '/{0}_gripper_controller/gripper_action'.format(self.direction)
-            gripper_client = SimpleActionClient(name_space, GripperCommandAction)
+            name_space = '/{0}_gripper_controller/gripper_action'.format(
+			self.direction)
+            gripper_client = SimpleActionClient(name_space,
+			GripperCommandAction)
             gripper_client.wait_for_server()
 
             gripper_goal = GripperCommandGoal()
             
-            if self.state:
-                 # The gripper is open. Close it and update its state.
+	    if self.state:
+                # The gripper is open. Close it and update its state.
                 gripper_goal.command.position = 0.0
                 self.state = Gripper.CLOSED
             else:
                 # The gripper is closed. Open it and update its state.
-                gripper_goal.command.position = 10.0
+                gripper_goal.command.position = 0.08
                 self.state = Gripper.OPEN
 
             gripper_goal.command.max_effort = 30.0
 
             gripper_client.send_goal(gripper_goal)
-            gripper_client.wait_for_result(rospy.Duration(100.0))
-
-            if (gripper_client.get_state() != GoalStatus.SUCCEEDED):
-                rospy.logwarn('Gripper action unsuccessful.')
-            print ("moved gripper!")  # TODO remove this
-
-        return move_gripper
-
+            gripper_client.wait_for_result(rospy.Duration(10.0))
+               
+	return move_gripper
 
  
 
