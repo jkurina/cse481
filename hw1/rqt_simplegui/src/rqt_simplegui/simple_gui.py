2a600c23137e4a11d531dea87507cd757e757c19
team2 <team2@shared.com>
2013-10-14 15:27:15 -0700

    Made gui prettyyy

--- hw1/rqt_simplegui/src/rqt_simplegui/simple_gui.py
+++ hw1/rqt_simplegui/src/rqt_simplegui/simple_gui.py
@@ -33,7 +33,7 @@ class SimpleGUI(Plugin):
         self.sound_sig.connect(self.sound_sig_cb)
         
         large_box = QtGui.QVBoxLayout()
-        
+
         #Sound textbox
         sound_textbox = QtGui.QLineEdit("Squirtle Squirtle") #Default Text
         sound_textbox.setFixedWidth(400)
@@ -45,54 +45,8 @@ class SimpleGUI(Plugin):
     	button_box.addWidget(sound_textbox)
         button_box.addStretch(1)
         large_box.addLayout(button_box)
-        large_box.addItem(QtGui.QSpacerItem(100,20))
-        
-        gripper = Gripper(Gripper.RIGHT, Gripper.OPEN)
-        right_gripper = self.create_button('Right gripper!', gripper.create_closure())
-        gripper = Gripper(Gripper.LEFT, Gripper.OPEN)
-        left_gripper = self.create_button('Left gripper!', gripper.create_closure()) 
-        large_box.addWidget(left_gripper)
-        large_box.addWidget(right_gripper)
 
-        up_head = Head(Head.UP)
-        up_head_button = self.create_button('up head', up_head.create_closure())
-        large_box.addWidget(up_head_button)
-        down_head = Head(Head.DOWN)
-        down_head_button = self.create_button('down head', down_head.create_closure())
-        large_box.addWidget(down_head_button)
-        right_head = Head(Head.RIGHT)
-        right_head_button = self.create_button('right head', right_head.create_closure())
-        large_box.addWidget(right_head_button)
-        left_head = Head(Head.LEFT)
-        left_head_button = self.create_button('left head', left_head.create_closure())
-        large_box.addWidget(left_head_button)
-
-        #forward
-        forward_base = Base(Base.FORWARD)
-        forward_base_button = self.create_button('move forward', forward_base.create_closure())
-        large_box.addWidget(forward_base_button)
-        #backward
-        backward_base= Base(Base.BACKWARD)
-      	backward_base_button = self.create_button('move backward', backward_base.create_closure())
-        large_box.addWidget(backward_base_button)
-        #left
-        left_base= Base(Base.LEFT)
-      	left_base_button = self.create_button('move left', left_base.create_closure())
-        large_box.addWidget(left_base_button)
-        #right
-        right_base= Base(Base.RIGHT)
-      	right_base_button= self.create_button('move right', right_base.create_closure())
-        large_box.addWidget(right_base_button)
-        #turn left
-        turnleft_base= Base(Base.TURNLEFT)
-      	turnleft_base_button = self.create_button('turn left', turnleft_base.create_closure())
-        large_box.addWidget(turnleft_base_button)
-        #turn right
-        turnright_base= Base(Base.TURNRIGHT)
-      	turnright_base_button = self.create_button('turn right', turnright_base.create_closure())
-        large_box.addWidget(turnright_base_button)
-
-        speech_box = QtGui.QHBoxLayout()
+	speech_box = QtGui.QHBoxLayout()
         self.speech_label = QtGui.QLabel('Robot has not spoken yet')
         palette = QtGui.QPalette()
         palette.setColor(QtGui.QPalette.Foreground,QtCore.Qt.blue)
@@ -102,10 +56,116 @@ class SimpleGUI(Plugin):
         large_box.addLayout(speech_box)
         large_box.addStretch(1)
 
+	large_box.addItem(QtGui.QSpacerItem(100,20))
+
+        up_head = Head(Head.UP)
+	head_box = QtGui.QVBoxLayout()
+	up_head_box = QtGui.QHBoxLayout()
+        up_head_button = self.create_button('^', up_head.create_closure())
+        #large_box.addWidget(up_head_button)
+        down_head = Head(Head.DOWN)
+	down_head_box = QtGui.QHBoxLayout()
+        down_head_button = self.create_button('v', down_head.create_closure())
+        #large_box.addWidget(down_head_button)
+        right_head = Head(Head.RIGHT)
+        right_head_button = self.create_button('>', right_head.create_closure())
+        #large_box.addWidget(right_head_button)
+        left_head = Head(Head.LEFT)
+        left_head_button = self.create_button('<', left_head.create_closure())
+	left_right_head_box = QtGui.QHBoxLayout()
+
+	up_head_box.addItem(QtGui.QSpacerItem(400,20))
+	up_head_box.addWidget(up_head_button)
+	up_head_box.addItem(QtGui.QSpacerItem(400,20))
+	left_right_head_box.addItem(QtGui.QSpacerItem(300,20))
+	left_right_head_box.addWidget(left_head_button)
+	left_right_head_box.addItem(QtGui.QSpacerItem(50,20))
+	left_right_head_box.addWidget(right_head_button)
+	left_right_head_box.addItem(QtGui.QSpacerItem(300,20))
+	down_head_box.addItem(QtGui.QSpacerItem(400,20))
+	down_head_box.addWidget(down_head_button)
+	down_head_box.addItem(QtGui.QSpacerItem(400,20))
+	head_box.addLayout(up_head_box)
+	head_box.addLayout(left_right_head_box)
+	head_box.addLayout(down_head_box)
+	large_box.addLayout(head_box)
+	#large_box.addItem(QtGui.QSpacerItem(500,20))
+        #large_box.addWidget(left_head_button)
+
+        gripper = Gripper(Gripper.RIGHT, Gripper.OPEN)
+        right_gripper = self.create_button('Right gripper!', gripper.create_closure())
+        gripper = Gripper(Gripper.LEFT, Gripper.OPEN)
+        left_gripper = self.create_button('Left gripper!', gripper.create_closure()) 
+	gripper_box = QtGui.QHBoxLayout()
+        gripper_box.addWidget(left_gripper)
+	gripper_box.addItem(QtGui.QSpacerItem(500,20))
+        gripper_box.addWidget(right_gripper)
+        large_box.addLayout(gripper_box)
+	large_box.addItem(QtGui.QSpacerItem(100,200))
+
+	base_box = QtGui.QVBoxLayout()
+
+        #forward
+	forward_base_box = QtGui.QHBoxLayout()
+        forward_base = Base(Base.FORWARD)
+        forward_base_button = self.create_button('move forward', forward_base.create_closure())
+	forward_base_box.addItem(QtGui.QSpacerItem(400,20))
+	forward_base_box.addWidget(forward_base_button)
+	forward_base_box.addItem(QtGui.QSpacerItem(400,20))
+	base_box.addLayout(forward_base_box)
+        #large_box.addWidget(forward_base_button)
+
+        #left
+	left_right_base_box = QtGui.QHBoxLayout()
+        left_base= Base(Base.LEFT)
+      	left_base_button = self.create_button('move left', left_base.create_closure())
+        #large_box.addWidget(left_base_button)
+
+        #right
+        right_base= Base(Base.RIGHT)
+      	right_base_button= self.create_button('move right', right_base.create_closure())
+	left_right_base_box.addItem(QtGui.QSpacerItem(300,20))
+	left_right_base_box.addWidget(left_base_button)
+	left_right_base_box.addItem(QtGui.QSpacerItem(50,20))
+	left_right_base_box.addWidget(right_base_button)
+	left_right_base_box.addItem(QtGui.QSpacerItem(300,20))
+        base_box.addLayout(left_right_base_box)
+	#large_box.addWidget(right_base_button)
+
+        #backward
+	backward_base_box = QtGui.QHBoxLayout()
+        backward_base= Base(Base.BACKWARD)
+      	backward_base_button = self.create_button('move backward', backward_base.create_closure())
+	backward_base_box.addItem(QtGui.QSpacerItem(400,20))
+	backward_base_box.addWidget(backward_base_button)
+	backward_base_box.addItem(QtGui.QSpacerItem(400,20))
+        base_box.addLayout(backward_base_box)
+	#large_box.addWidget(backward_base_button)
+
+	large_box.addLayout(base_box)
+        
+	turn_base_box = QtGui.QHBoxLayout()
+
+	#turn left
+        turnleft_base= Base(Base.TURNLEFT)
+      	turnleft_base_button = self.create_button('        /\n<--', turnleft_base.create_closure())
+	#large_box.addWidget(turnleft_base_button)
+        
+	#turn right
+        turnright_base= Base(Base.TURNRIGHT)
+      	turnright_base_button = self.create_button('\\\n        -->', turnright_base.create_closure())
+	turn_base_box.addItem(QtGui.QSpacerItem(300,20))
+	turn_base_box.addWidget(turnright_base_button)
+	turn_base_box.addItem(QtGui.QSpacerItem(100,20))
+	turn_base_box.addWidget(turnleft_base_button)
+	turn_base_box.addItem(QtGui.QSpacerItem(300,20))
+	large_box.addLayout(turn_base_box)
+	#large_box.addWidget(turnright_base_button)
+
         self._widget.setObjectName('SimpleGUI')
         self._widget.setLayout(large_box)
         context.add_widget(self._widget)
-        
+  
     def sound_cb(self, sound_request):
         qWarning('Received sound.')
         self.sound_sig.emit(sound_request)
