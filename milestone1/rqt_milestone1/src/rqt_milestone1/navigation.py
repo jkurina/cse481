5b1b3832b7c43ff9d1dd73390f0ec71397a4d6f2
Vimala <vjampala@cs.washington.edu>
2013-11-20 14:44:49 -0800

    modified navigation code to depend on book

--- milestone1/rqt_milestone1/src/rqt_milestone1/navigation.py
+++ milestone1/rqt_milestone1/src/rqt_milestone1/navigation.py
@@ -10,17 +10,17 @@ from actionlib_msgs.msg import GoalStatus
 from geometry_msgs.msg import PoseStamped
 
 # Navigates the PR2 to the location of the bookshelf
-def move_to_shelf():
+def move_to_shelf(x, y):
     pub = rospy.Publisher('/move_base_simple/goal', PoseStamped)
     msg = PoseStamped()
     msg.header.frame_id = "map"
     msg.header.stamp = rospy.Time.now()
-    msg.pose.position.x = 0.108998298645
-    msg.pose.position.y = -2.0217675209
+    msg.pose.position.x = x
+    msg.pose.position.y = y
     msg.pose.orientation.z = 1.353879350922
     msg.pose.orientation.w = -0.300190246152
     pub.publish(msg)
-    rospy.loginfo("Navigated to the bookshelf")
+    rospy.loginfo("Navigated to " + str(x) + ", " + str(y))
 
 # book id 14
 #    msg.pose.position.x = 0.780967617035
