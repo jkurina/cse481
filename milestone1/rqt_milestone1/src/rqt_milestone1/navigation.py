6c646d099bc910b768fd6b7b996f32d69ae08cc9
vjampala <vjampala@cs.washington.edu>
2013-11-27 16:48:37 -0800

    Added new book positions

--- milestone1/rqt_milestone1/src/rqt_milestone1/navigation.py
+++ milestone1/rqt_milestone1/src/rqt_milestone1/navigation.py
@@ -17,7 +17,7 @@ class Navigation():
 	    self.client.wait_for_server()
 
 	# Navigates the PR2 to the location passed in
-	def move_to_shelf(self, x, y, z=0.990642343157, w=-0.136483507958):
+	def move_to_shelf(self, x, y, z=0.993642343157, w=-0.111811803131):
 	    goal = MoveBaseGoal()
 	    goal.target_pose.header.frame_id = "map"
 	    goal.target_pose.header.stamp = rospy.Time.now()
@@ -25,12 +25,6 @@ class Navigation():
 	    goal.target_pose.pose.position.y = y
 	    goal.target_pose.pose.orientation.z = z
 	    goal.target_pose.pose.orientation.w = w
-#middle book: 
-#    x: 0.905410051346
-#    y: -2.0606584549
-# first book
-#    x = 1.54238247871
-#    y = -1.83420526981
 
 	    # Send the goal to the action server
 	    self.client.send_goal(goal)
@@ -38,12 +32,9 @@ class Navigation():
 
 	    # Wait for the robot to navigate to the specified location
 	    self.client.wait_for_result()
-            #print (self.client.get_state())
- 	    print ("Result: " + str(self.client.get_result()))
 	    # Check if the navigation was successful
 	    if (self.client.get_state() != GoalStatus.SUCCEEDED):
 		rospy.loginfo("PR2's navigation was unsuccessful")
 	    else:
 		rospy.loginfo("PR2 navigated successfully")
-		print ("State: " + str(self.client.get_state()))
 
